39495a37a5c51b179c07614357a5be2c829ad945
Victor Turitzin <turitzinv@gmail.com>
2021-12-05 14:43:51 -0500

    commit tree bf7e60c3604d501742b7354f3dcae029b2590b91

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+/*function superbowlWin(array) {
+  if (array.result === "W"){
+    return array.year
+  } else {
+    return "undefined"
+  }
+} */
 
+/*function superbowlWin(array){
+    for(let result of array){
+      if (array.result === "W"){
+        return array.year
+      } else {
+        return "undefined"
+      }
+    }
+  }*/
+
+/*
+1. Create an if statement that checks to see if we in fact have an object
+
+2. If we do:
+Return that object’s year
+
+3. If not:
+Return undefined 
+*/
+
+  /* function superbowlWin(array){
+      const winYear = array.find(record => record.result==="W")
+      console.log('This is the win', winYear)
+      return winYear.year
+  } */
+
+  function superbowlWin(array){
+    const winYear = array.find(record => record.result==="W")
+    if (typeof winYear === "object"){
+        return winYear.year
+    } else {
+        return undefined
+    }   
+}
